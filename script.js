e55d9ffd38c76d3364b7c7ad63bf17ec16deda84
Kurt Weber <kdweber89@gmail.com>
2015-03-17 16:33:48 -0400

    webpage for Hilde Hoffman

---
+++ script.js
@@ -0,0 +1,49 @@
+//ok......this is the hard stuff.....but you can do it!!!!!
+
+
+
+$(document).ready(function(){
+    /*$("#star").mouseenter(function(){
+        $(".circle").fadeIn();
+    });
+    $("#star").mouseleave(function(){
+        $(".circle").fadeOut();
+    });*/
+
+    $("#star1").mouseenter(function(){
+        $(".circle1").fadeIn();
+    });
+    $("#star1").mouseleave(function(){
+        $(".circle1").fadeOut();
+    });
+
+    $("#star2").mouseenter(function(){
+        $(".circle2").fadeIn();
+    });
+    $("#star2").mouseleave(function(){
+        $(".circle2").fadeOut();
+    });
+
+    $("#star3").mouseenter(function(){
+        $(".circle3").fadeIn();
+    });
+    $("#star3").mouseleave(function(){
+        $(".circle3").fadeOut();
+    });
+
+    $("#star4").mouseenter(function(){
+        $(".circle4").fadeIn();
+    });
+    $("#star4").mouseleave(function(){
+        $(".circle4").fadeOut();
+    });
+
+    $("#star5").mouseenter(function(){
+        $(".circle5").fadeIn();
+    });
+    $("#star5").mouseleave(function(){
+        $(".circle5").fadeOut();
+    });
+
+});
+
